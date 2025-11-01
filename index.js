d658d7f5c55dc7fc44209e96ddc3096cfe2e0118
Ayrton <ayrtongreenfield.dev@gmail.com>
2025-11-01 19:01:35 +0000

    next

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const burgerMenu = document.querySelector('#burger-menu');
-const navMenu = document.querySelector('#nav-menu');
+document.addEventListener("DOMContentLoaded", () => {
 
-burgerMenu.addEventListener('click', () => {
-    burgerMenu.classList.toggle('burger-change');
-    navMenu.classList.toggle('nav-active');
+    const burgerMenu = document.querySelector('#burger-menu');
+    const navMenu = document.querySelector('#nav-menu');
+
+    burgerMenu.addEventListener('click', () => {
+        burgerMenu.classList.toggle('burger-change');
+        navMenu.classList.toggle('nav-active');
+    });
+
+    const serviceCards = document.querySelectorAll('.service-card[data-href]');
+
+    serviceCards.forEach(card => {
+        card.addEventListener('click', () => {
+            const href = card.getAttribute('data-href');
+            if (href === "#telecommunications-surveys" || href === "#panoramic-surveys") {
+                window.location.href = "services.html#telecom-pano-surveys";
+            } else if (href === "#bespoke-builds" || href === "#prototyping") {
+                window.location.href = "services.html#bespoke-prototype";
+            } else {
+                window.location.href = "services.html" + href;
+            }
+        });
+    });
 });
-
